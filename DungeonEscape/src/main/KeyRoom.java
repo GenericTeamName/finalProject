e0be68d3c28e07ab407d3ac6b487bd7efe0b6538
hs137431 <31348463+hs137431@users.noreply.github.com>
2017-10-17 18:37:53 -0500

    update

--- DungeonEscape/src/main/KeyRoom.java
+++ DungeonEscape/src/main/KeyRoom.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 public class KeyRoom {
 
 	public static void main(String[] args) {
@@ -7,6 +9,15 @@ public class KeyRoom {
 		item();
 	}
 	public static void item() {
-		
+		String choice;
+		JOptionPane.showMessageDialog(null, "You find a golden key with a skull design on the base of it, just looking"
+				+ " at it you feel like it \n might fall apart it’s probably only going to work for one door.");
+		JOptionPane.showMessageDialog(null, "As you look around you find not only a door but a hole in the wall that "
+				+ "looks like you could climb through and a keyhole.");
+		choice = JOptionPane.showInputDialog("You must make a choice do you \n1 climb through the hole in the wall \n2 "
+				+ "Use the key on the keyhole");
+		if ("1".equalsIgnoreCase(choice)) {
+			  
+		}
 	}
 }
